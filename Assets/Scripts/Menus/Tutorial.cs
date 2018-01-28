efdaf7090faf046fe72453c6008a1de04bf4a77f
Pedro Camara <mimpedro@gmail.com>
2018-01-28 02:46:30 -0200

    Added main menu and tutorial screen logic

---
+++ Assets/Scripts/Menus/Tutorial.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Tutorial : MonoBehaviour {
+    public Sprite[] images;
+    public Text pageText;
+    public int currentPage = 0;
+    public GameObject previousButton;
+    public Image displayImage;
+
+	// Use this for initialization
+	void Start () {
+        updateTutorialImage();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        pageText.text = (currentPage + 1) + "/" + images.Length;
+	}
+
+    public void AdvancePage(int increment)
+    {
+        currentPage += increment;
+        if (currentPage == 0)
+        {
+            previousButton.SetActive(false);
+        }
+        else if (!previousButton.activeSelf)
+        {
+            previousButton.SetActive(true);
+        }
+        if (currentPage >= images.Length)
+        {
+            StartGame();
+        }
+        updateTutorialImage();
+    }
+
+    void updateTutorialImage()
+    {
+        displayImage.sprite = images[currentPage];
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene("TextTest");
+    }
+}
