15e47ebbefe5517db9281532ec372a31b7b7d65b
Pedro Camara <mimpedro@gmail.com>
2018-01-28 04:15:45 -0200

    Created working fastForward button - needs to be integrated at scene.

--- Assets/Scripts/ScrollingBehaviour.cs
+++ Assets/Scripts/ScrollingBehaviour.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ScrollingBehaviour : MonoBehaviour {
-
+    public float normalSpeed;
+    public float fastSpeed;
+    [HideInInspector]
+    public float currentSpeed;
 	// Use this for initialization
 	void Start () {
-		
+        currentSpeed = normalSpeed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position += new Vector3(0, 0.2f, 0);
+        transform.position += new Vector3(0, currentSpeed, 0);
 	}
+    public void SetSpeed(string speed)
+    {
+        switch (speed)
+        {
+            case "normal":
+                currentSpeed = normalSpeed;
+                break;
+            case "fast":
+                currentSpeed = fastSpeed;
+                break;
+        }
+    }
 }
